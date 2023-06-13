347550b8e099ed8a9e875940d3602168b2742bae
Vu Huong Khon <vu.huongkhon@nashtechglobal.com>
2023-06-14 04:21:08 +0700

    Add book form

---
+++ Solution/BookManagement.UI/BookManagement.UI.Blazor/Shared/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace BookManagement.UI.Blazor.Shared
+{
+	public static class Constants
+	{
+		public static List<NavItem> SidebarItems { get; } = new List<NavItem>
+	{
+		new NavItem { Link = "/", Title = "Home" },
+		new NavItem { Link = "/products", Title = "Products" },
+		new NavItem { Link = "/about", Title = "About" }
+	};
+	}
+
+	public class NavItem
+	{
+		public string Link { get; set; }
+		public string Title { get; set; }
+	}
+
+}
